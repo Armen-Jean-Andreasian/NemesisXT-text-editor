e51d74a7fe998f7b10a22df6560eb2c117f4a649
Armen-Jean-Andreasian <armen.andriasyan4@gmail.com>
2023-10-14 10:49:14 +0700

    Initial

--- src/modules/editor_app_main.py
+++ src/modules/editor_app_main.py
@@ -23,7 +23,7 @@ class TextEditor:
     """
 
     __THEMES_FILEPATH = os.path.abspath(os.path.join(os.getcwd(), "files/themes.json"))
-    __LOGO_FILEPATH = os.path.abspath(os.path.join(os.getcwd(), "files/token.ico"))
+    __LOGO_FILEPATH = os.path.abspath(os.path.join(os.getcwd(), "files/window_logo.png"))
 
     def __init__(self, filepath_=None, logo_filepath=None):
         if filepath_:
@@ -77,5 +77,5 @@ class TextEditor:
 
 
 if __name__ == '__main__':
-    app = TextEditor(filepath_='../../files/themes.json', logo_filepath='../../files/logo.ico')
+    app = TextEditor(filepath_='../../files/themes.json', logo_filepath='../../files/window_logo.png')
     app.root.mainloop()
